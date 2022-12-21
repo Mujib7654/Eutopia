c0c2f3111cc1674be8b355ff829e101e663c2048
Ehtesham Faraz <efaraz27@gmail.com>
2022-12-22 01:23:19 +0530

    add: event list

---
+++ server/src/constants/events.ts
@@ -0,0 +1,42 @@
+const EVENTS = [
+  {
+    name: "event1",
+    organizer: "organizer1",
+    fees: 100,
+    memberCount: 10,
+    coordinators: [
+      {
+        name: "coordinator1",
+        phone: "1234567890",
+      },
+    ],
+    introduction: `introduction1`,
+    rules: `rules1`,
+    bannerUrl: "bannerUrl1",
+    organizerLogoUrl: "organizerLogoUrl1",
+  },
+  {
+    name: "What if?",
+    organizer: "Theatrix",
+    fees: 200,
+    memberCount: 4,
+    coordinators: [
+      {
+        name: "Abhranil Roy",
+        phone: "7044992461",
+      },
+      {
+        name: "Snehatit Sett",
+        phone: "9836729146",
+      },
+      {
+        name: "Srijani Dasgupta",
+        phone: "9830896527",
+      },
+    ],
+    introduction: `"In dreams, we enter, a world that's entirely our own" ~ Albus Dumbledore Have you ever wanted to barge into the screen writer's office of a movie that you loved and just wanted to take over the narration. Well, you've come to the right place!! Because here we are giving you the chance to let your imagination run wild. What if Augustus Rockwood never killed Fred? अगर देवदा स ने चंद्रमुखी की बा त मा न ली हो ती , तो क्या हो ता ? যদি সে মন্তী সরকা র সো না দা র কা ছে ইসলা মী ক ইতি হা স নি য়ে না পড়ত তবে কে মন হতো তুমি বলতো ? With a swish and flick of your pen you can have your own exclusive storyline. All you have to do is ask WHAT IF?`,
+    rules: `Each team will consist of 4 members (schools and colleges inclusive) who'll take any famous scene from any famous story/movie/book etc and have to alter the storyline and perform. Example: 1) Chitrangada and Arjun story 2) If Aman Mathur never dies in Kal ho na ho etc 3) What if the magic of Gupi-Bagha stops working ? The winner will be that team who'll create the best storyline and act wonderfully. They will be judged on the basis of these points: 1) Storyline 2) Acting 3) Timing 4) Audience reaction`,
+    bannerUrl: null,
+    organizerLogoUrl: null,
+  },
+];
