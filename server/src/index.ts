bc687704057b52385706a962971d58ce19b58adf
Ehtesham Faraz <efaraz27@gmail.com>
2023-01-04 00:08:19 +0530

    envs

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,8 +5,6 @@ import cors from "cors";
 import morgan from "morgan";
 import dotenv from "dotenv";
 import FirebaseAdmin, { ServiceAccount } from "firebase-admin";
-import keys from "./secrets/keys.json";
-import firebaseConfig from "./secrets/firebase-config.json";
 import { router as AuthRouter } from "./routes/auth";
 import { router as ResgisterRouter } from "./routes/register";
 import { router as EventRouter } from "./routes/event";
@@ -22,18 +20,32 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 8000;
 
+const FirebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
+};
+
+console.log(FirebaseConfig);
+
 try {
   const serviceAccount: ServiceAccount = {
-    projectId: keys.project_id,
-    privateKey: keys.private_key,
-    clientEmail: keys.client_email,
+    projectId: process.env.PROJECT_ID,
+    privateKey: process.env.PRIVATE_KEY,
+    clientEmail: process.env.CLIENT_EMAIL,
   };
+
+  console.log(serviceAccount);
 
   FirebaseAdmin.initializeApp({
     credential: FirebaseAdmin.credential.cert(serviceAccount),
   });
 
-  initializeApp(firebaseConfig);
+  initializeApp(FirebaseConfig);
 } catch (err) {
   console.error(err);
 }
